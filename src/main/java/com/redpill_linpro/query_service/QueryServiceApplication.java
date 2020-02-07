fe973b30f2d6e36a48a491874aeb385c5763c6ec
chriakri100 <chriakri100@student.kau.se>
2020-02-07 16:01:56 +0100

    Initial Commit

---
+++ src/main/java/com/redpill_linpro/query_service/QueryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.redpill_linpro.query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryServiceApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/com/redpill_linpro/query_service/QueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redpill_linpro.query_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
