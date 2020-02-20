20cc5406941ef7f2eb609328db560df2d9b676c0
Simon Tham <simon.tham@live.com>
2020-02-20 13:24:53 +0100

    Add function to send query to graphDB

---
+++ src/main/java/com/redpill_linpro/query_service/GraphDBHandler.java
@@ -0,0 +1,35 @@
+package com.redpill_linpro.query_service;
+
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.query.*;
+import org.eclipse.rdf4j.repository.http.HTTPRepository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+
+public class GraphDBHandler {
+    private RepositoryConnection connection;
+
+    public GraphDBHandler() {
+        connectRepository("http://localhost:7200/repositories/LearningGraphDB");
+    }
+
+    private void sendQuery(String query) {
+        TupleQuery tupleQuery = connection.prepareTupleQuery(QueryLanguage.SPARQL, query);
+
+        TupleQueryResult tupleQueryResult = tupleQuery.evaluate();
+        while (tupleQueryResult.hasNext()) {
+            BindingSet bindingSet = tupleQueryResult.next();
+
+            for (Binding binding : bindingSet) {
+                String name = binding.getName();
+                Value value = binding.getValue();
+
+                System.out.println(name + " = " + value);
+            }
+        }
+    }
+
+    private void connectRepository(String repo){
+        HTTPRepository repository = new HTTPRepository(repo);
+        connection = repository.getConnection();
+    }
+}
