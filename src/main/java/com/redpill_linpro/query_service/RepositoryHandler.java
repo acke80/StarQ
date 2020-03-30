c60ece976c86fc34efaff29f14988b056b8467b7
Simon Tham <simon.tham@live.com>
2020-03-30 09:35:25 +0200

    Replace FedXFactory and add html

--- src/main/java/com/redpill_linpro/query_service/RepositoryHandler.java
+++ src/main/java/com/redpill_linpro/query_service/RepositoryHandler.java
@@ -4,44 +4,45 @@ import org.eclipse.rdf4j.federated.FedXFactory;
 import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.query.*;
 import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryException;
 import org.eclipse.rdf4j.repository.http.HTTPRepository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public final class RepositoryHandler {
-    private static RepositoryConnection connection;
-    private static final Repository repository = FedXFactory.newFederation()
-            .withSparqlEndpoint("http://localhost:7200/repositories/repoGraphDB")
-            .create();
+public class RepositoryHandler {
+    private RepositoryConnection connection;
+    private final Repository repository = new HTTPRepository("http://localhost:7200/repositories/LearningGraphDB");
 
-    private RepositoryHandler() { }
+    public RepositoryHandler() { }
 
     /**
      * Sends a query to a SPARQL endpoint and returns the data asked for in the query
      * @param text The query that will be sent to the endpoint
      * @return A list of names and values
-     * @throws Exception
+     * @throws RepositoryException
      */
-    public static List<String> sendQuery(String text) throws Exception{
+    public List<String> sendQuery(String text) throws RepositoryException {
         List<String> bindings = new ArrayList<>();
 
         try {
             connection = repository.getConnection();
 
-            TupleQuery tupleQuery = connection.prepareTupleQuery(text);
+            TupleQuery tupleQuery = connection.prepareTupleQuery(QueryLanguage.SPARQL, text);
 
             TupleQueryResult tupleQueryResult = tupleQuery.evaluate();
 
             while (tupleQueryResult.hasNext()) {
                 BindingSet bindingSet = tupleQueryResult.next();
+                System.out.println(bindingSet);
 
                 for (Binding binding : bindingSet) {
                     String name = binding.getName();
                     Value value = binding.getValue();
 
                     bindings.add(name + " = " + value);
+                    System.out.println(name + " = " + value);
                 }
             }
         }finally{
